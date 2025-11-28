7a2e1b358bb5780b9b6a584c80a88a617471ae63
Lorenz Sichert <lorenz.sichert@proton.me>
2025-11-28 17:03:45 +0100

    final

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,45 +1,4 @@
--- load defaults i.e lua_lsp
 require("nvchad.configs.lspconfig").defaults()
 
-local lspconfig = require "lspconfig"
-
--- EXAMPLE
-local servers = { "html", "cssls" }
-local nvlsp = require "nvchad.configs.lspconfig"
-
--- lsps with default config
-for _, lsp in ipairs(servers) do
-  lspconfig[lsp].setup {
-    on_attach = nvlsp.on_attach,
-    on_init = nvlsp.on_init,
-    capabilities = nvlsp.capabilities,
-  }
-end
-
-
--- Lsp Servers
-lspconfig.rust_analyzer.setup {
-  on_attach = nvlsp.on_attach,
-  on_init = nvlsp.on_init,
-  capabilities = nvlsp.capabilities,
-}
-
-lspconfig.jdtls.setup {
-  on_attach = nvlsp.on_attach,
-  on_init = nvlsp.on_init,
-  capabilities = nvlsp.capabilities,
-}
-
-
-vim.diagnostic.config({
-  -- disable virtual text
-  virtual_text = false,
-  update_in_insert = true,
-  underline = false,
-  severity_sort = true,
-  float = {
-    style = "minimal",
-    header = ""
-  }
-})
-
+local servers = { "html", "cssls", "pyright" }
+vim.lsp.enable(servers)
